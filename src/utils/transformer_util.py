17df4c0060fc670f6f186747350f6121b3cfdcb7
Ketan Parekh <p.ketan@gmail.com>
2024-11-30 14:07:12 -0500

    refactored code and modified pipe to return trained model and prediction dataset

--- src/utils/transformer_util.py
+++ src/utils/transformer_util.py
@@ -1,8 +1,13 @@
 import pandas as pd
 
-
-
 def encode_data(data: pd.DataFrame):
+    """Encode the rating_bin column using LabelEncoder.
+    Args:
+        data (pd.DataFrame): DataFrame containing the rating_bin column.
+
+    Returns:
+        pd.DataFrame: Category encoded DataFrame.
+    """
     # LabelEncode rating_bin column
     data['rating_bin'] = data['rating_bin'].astype('category')
     data['rating_bin'] = data['rating_bin'].cat.codes
